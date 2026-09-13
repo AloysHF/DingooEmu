f57c9339e6021cd37c4bd694c14b0813a9b5ee72
Aloys <jiangxinnju@163.com>
2026-09-13 14:22:16 +0800

    chore: bump version to 0.3.0

--- crates/dingooemu-libretro/src/api.rs
+++ crates/dingooemu-libretro/src/api.rs
@@ -82,7 +82,7 @@ pub extern "C" fn retro_get_system_info(info: *mut RetroSystemInfo) {
     };
 
     info.library_name = c"DingooEmu".as_ptr();
-    info.library_version = c"0.2.0".as_ptr();
+    info.library_version = c"0.3.0".as_ptr();
     info.valid_extensions = c"app|cc|c2s|c3s".as_ptr();
     info.need_fullpath = true;
     info.block_extract = false;
